1a117c017f2299a85f5b1542f04bb65a578c80aa
NoviceZhan <158635978@qq.com>
2018-11-23 15:47:37 +0800

    修改后的ProductC.java文件

--- src/com/zhb/c/ProductC.java
+++ src/com/zhb/c/ProductC.java
@@ -1,4 +1,4 @@
-package com.zhb.c;
+﻿package com.zhb.c;
 
 
 import java.util.List;
@@ -14,6 +14,7 @@ import com.zhb.service.ProductService;
  * @开发人员:占沪斌
  * @创建时间:2018年9月7日上午11:45:50
  */
+//测试
 @Controller
 public class ProductC {
 	@Autowired
